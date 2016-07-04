771641be1fd4fd204d9dbc8062a2b96d17739403
Manuel Odendahl <wesen@ruinwesen.com>
2016-07-04 19:03:05 +0200

    Remove PROGMEM from keymap_chaosghoul

--- keymap_chaosghoul.c
+++ keymap_chaosghoul.c
@@ -1,6 +1,6 @@
 #include "keymap_common.h"
 
-const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+const uint8_t keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /* 0: qwerty */
     KEYMAP(ESC,  F1,   F2,  F3,  F4,  F5,  F6,  F7,  F8,  F9,   F10,  F11,  F12,  PSCR, SLCK, PAUS, PWR, \
            GRV,  1,    2,   3,   4,   5,   6,   7,   8,   9,    0,    MINS, EQL,  BSLS, BSPC, INS,  HOME, PGUP, NLCK, PSLS, PAST, PMNS, \
@@ -9,4 +9,4 @@ const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
            LSFT, BSLS, Z,   X,   C,   V,   B,   N,   M,   COMM, DOT,  SLSH, RSFT, UP,  P1,    P2,   P3, \
            LCTL, LGUI,LALT,           SPC,                      RALT, RGUI, APP, RCTL, LEFT,  DOWN, RGHT, P0, PCMM, PENT ),
 };
-const uint16_t PROGMEM fn_actions[] = {};
+const uint16_t fn_actions[] = {};
